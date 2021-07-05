917d7bd91b496bd89f42286339c3d5d2a053faa3
Omri Cohen <omri_cohen@outlook.com>
2021-07-05 16:54:36 +0300

    Added basic iterate logic

---
+++ iterate.py
@@ -0,0 +1,12 @@
+def iterate(grid: list) -> list:
+    #Foreach cell:
+        # If alive:
+            # If (live_neighbors is 2 or 3):
+                #alive
+            # else:
+                #dead
+        # else:
+            # if(live_neighbors is 3):
+                # alive
+            # else:
+                # dead 

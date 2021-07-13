086790367e95c72a9a88d020e3903bb2d21edb82
Omri Cohen <omri_cohen@outlook.com>
2021-07-13 21:59:20 +0300

    finished refreshing display

--- display.py
+++ display.py
@@ -1,10 +1,16 @@
 import sys
+from console.screen import sc
 
-def display_grid(grid: list):
-    for i, row in enumerate(grid):
-        for j, cell in enumerate(row):
-            if(cell):
-                print("X", end="")
-            else:
-                print("0", end="")
-        print("\r")
+class ConsoleDisplay():
+    def __init__(self):
+        pass
+
+    def display_grid(self, grid: list):
+        with sc.location(0, 0):
+            for i, row in enumerate(grid):
+                for j, cell in enumerate(row):
+                    if(cell):
+                        print("X", end = "")
+                    else:
+                        print("0", end="")
+                print("")
--- gameoflife.py
+++ gameoflife.py
@@ -1,11 +1,12 @@
-from display import display_grid
+from display import ConsoleDisplay
 from iterate import fetch_live_neighbors, iterate
 from input_handler import read_grid_from_file
 import time
 
 grid = read_grid_from_file("grid.txt")
+display = ConsoleDisplay()
 
 for i in range(100):
-    display_grid(grid)
+    display.display_grid(grid)
     time.sleep(0.1)
     grid = iterate(grid)

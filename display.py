ed27921d0ff21c7fa38366dd2e704d32b5b445a4
Omri Cohen <omri_cohen@outlook.com>
2021-07-13 21:33:01 +0300

    added gitignore

--- display.py
+++ display.py
@@ -1,2 +1,13 @@
+import sys
+
 def display_grid(grid: list):
-    for i, row:
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if(cell):
+                sys.stdout.write("X")
+            else:
+                sys.stdout.write("0")
+        sys.stdout.write("\n")
+
+def flush_screen():
+    sys.stdout.flush()
--- gameoflife.py
+++ gameoflife.py
@@ -1,14 +1,19 @@
+from display import display_grid
 from iterate import fetch_live_neighbors, iterate
 from input_handler import read_grid_from_file
-
+import time
 
 grid = read_grid_from_file("grid.txt")
-
+display_grid(grid)
+time.sleep(0.1)
 grid = iterate(grid)
-print(grid)
+display_grid(grid)
+time.sleep(0.1)
 grid = iterate(grid)
-print(grid)
+display_grid(grid)
+time.sleep(0.1)
 grid = iterate(grid)
-print(grid)
+display_grid(grid)
+time.sleep(0.1)
 grid = iterate(grid)
-print(grid)
+display_grid(grid)

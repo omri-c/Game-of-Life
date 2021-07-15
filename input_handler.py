5e28dd616ac1fcd59d034d3cc36ed1c2d47cd06b
Omri Cohen <omri_cohen@outlook.com>
2021-07-16 00:00:08 +0300

    removed need for commas

--- input_handler.py
+++ input_handler.py
@@ -3,7 +3,7 @@ def read_grid_from_file(path: str) -> list:
     with open(path, 'r') as f:
         lines = f.readlines()
         for l in lines:
-            l = l.split(",")
+            #l = l.split(",")
             new_line = [(x in ('1', '1\n')) for x in l]
             grid.append(new_line)
         
